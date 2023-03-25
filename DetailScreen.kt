d497cc0d722ca30ac730aedaaaffc18003350464
MohamadArastu42 <93126928+MohamadArastu42@users.noreply.github.com>
2023-03-25 15:52:11 +0100

    Add files via upload
    Finished

---
+++ DetailScreen.kt
@@ -0,0 +1,13 @@
+package com.example.lectureexamples.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.lectureexamples.models.getMovies
+
+@Composable
+fun DetailScreen(navController: NavController, movieId: String?) {
+    movieId?.let {
+        Text(text = "$movieId")
+    }
+}
